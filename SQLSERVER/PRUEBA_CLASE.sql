01a7194e3f9b55997ad82394767553a7a167df6b
ricardo_jonel_15@hotmail.com <ricardo_jonel_15@hotmail.com>
2017-03-05 18:02:10 -0500

    html5

--- SQLSERVER/PRUEBA_CLASE.sql
+++ SQLSERVER/PRUEBA_CLASE.sql
@@ -1,11 +1,17 @@
 SELECT * FROM Customer;
+select * from [dbo].[Order]
+OrderByOrderNumber
+SELECT * FROM [dbo].[Order] WHERE ID= 830
+SELECT * FROM [dbo].[OrderItem] WHERE OrderId = 830
+
+ dbo.OrderWithOrderItems	
 
 -- dbo.CustomerWithOrders 1
 
 -- buscar las ordenes por OrderNumber
 --- una orden puede tener muchas items
 
--- 1. Crearel IOrderRepository
+-- 1. Crear el IOrderRepository
 -- 2. Implementar interfaz IOrderRepository
 -- 3. Modificar UnitOfWork para usar la interfaz creada.
 -- 4. Crear 2 SP,
