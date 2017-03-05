df92802e52cb9016c9f2af0cb70c085c7cb6d344
ricardo_jonel_15@hotmail.com <ricardo_jonel_15@hotmail.com>
2017-03-05 13:30:53 -0500

    prueba

--- Tibox.Repository.Tests/OrderRepositoryTest.cs
+++ Tibox.Repository.Tests/OrderRepositoryTest.cs
@@ -30,6 +30,62 @@ namespace Tibox.DataAccess.Tests
             Assert.AreEqual(lstOrders.Count() > 0, true);
         }
 
+
+
+        [TestMethod]
+        public void Insert_Orders()
+        {
+            var order = new Order { OrderDate = DateTime.Now,
+                    OrderNumber = "543207", CustomerId = 65, TotalAmount = 20 };
+            var resulta = _IUnitOfWork.Orders.Insert(order);
+        }
+
+        [TestMethod]
+        public void Find_Order()
+        {
+            //int id = 93;
+
+            //var customer = _UnitOfWork.GetEntityById(93);
+            var order = _IUnitOfWork.Orders.GetEntityById(830);
+            Assert.AreEqual(order != null, true);
+        }
+
+        [TestMethod]
+        public void Update_Orders()
+        {
+            var order = new Order
+            {
+                OrderDate = DateTime.Now,
+                OrderNumber = "543207",
+                CustomerId = 65,
+                TotalAmount = 90,
+                Id = 832
+            };
+            //var bResult = _Repository.UpdateCustomer(customer);
+            //var bResult = _UnitOfWork.Update(customer);
+            //var bResult = _UnitOfWork.Order.Update(order);
+            var bResult = _IUnitOfWork.Orders.Update(order);
+            Assert.AreEqual(bResult, true);
+        }
+
+        [TestMethod]
+        public void Delete_Customer()
+        {
+            var order = new Order
+            {
+                OrderDate = DateTime.Now,
+                OrderNumber = "543207",
+                CustomerId = 65,
+                TotalAmount = 20,
+                Id = 832
+            };
+            //var bResult = _Repository.DeleteCustomer(customer);
+            //var bResult = _UnitOfWork.Delete(customer);
+            //var bResult = _UnitOfWork.Orders.Delete(customer);
+            var bresulta = _IUnitOfWork.Orders.Delete(order);
+            Assert.AreEqual(bresulta, true);
+        }
+
         [TestMethod]
         public void Order_By_Number()
         {
